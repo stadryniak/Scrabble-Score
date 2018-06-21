823962a096e66403047ccc3e61e7157993dadaae
Michal Stadryniak <stadryniak.michal@gmail.com>
2018-06-21 18:30:54 +0200

    Improvemets
    Changelog:
    -better look in console
    -userproofness

--- ScrabbleScore.py
+++ ScrabbleScore.py
@@ -1,37 +1,55 @@
 def wczytaj_slownik():
     '''Wczytanie słownika'''
     print ('Inicjalizacja słownika...')
-    slownik=[]
     with open("slowa.txt", 'r', encoding='utf-8') as f:
         slownik=f.readlines()
     slownik=[x.strip() for x in slownik]
     print('Słownik gotowy')
+    print ('')
     return slownik
 
 def wprowadz_slowo():
-    slowo=input('Podaj slowo do sprawdzenia: ')
+    '''Wczytanie słowa od użytkownika'''
+    slowo=input('Podaj slowo do sprawdzenia lub 0, aby zakończyć: ')
+    print('')
+    slowo=slowo.lower()
     return slowo
 
 def wybor():
+    '''Odpowiada za wybór funkcji programu'''
     print ('Wybierz co chcesz zrobić')
     print ('[1] Sprawdzić czy istnieje')
     print ('[0] Wyjście')
+    print ('')
     try:
-        wybor=input('Decyzja: ')
-        return int(wybor)
+        wybor=int(input('Wybór: '))
+        print('')
+        if wybor>1:
+            print('Nieprawidłowy wybór')
+            print('')
+            return None
+        return wybor
     except ValueError:
-        print ('Nieprawidłowy wybór!')
-    return None
+        print('')
+        print ('Nieprawidłowy wybór')
+        print('')
+        return None
+
 
 slownik=wczytaj_slownik()
 wyb=-1
 while wyb!=0:
     sprawdz=wprowadz_slowo()
+    if sprawdz=='0':
+        print ('Koniec pracy')
+        break
     wyb=wybor()
     if wyb==1:
         if sprawdz in slownik: 
-            print ('Slowo istnieje')
+            print ('Słowo istnieje')
+            print('')
         else:
             print ('Słowo nie istnieje')
+            print('')
 
     
