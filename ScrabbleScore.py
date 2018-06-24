8b192da441d9257f85aea22ad91223d1d1339ba4
Michal Stadryniak <stadryniak.michal@gmail.com>
2018-06-24 15:35:53 +0200

    Extras
    Bugs added

--- ScrabbleScore.py
+++ ScrabbleScore.py
@@ -1,30 +1,32 @@
 def wczytaj_slownik():
     '''Wczytanie słownika'''
-    print ('Inicjalizacja słownika...')
+    print('Inicjalizacja słownika...')
     with open("slowa.txt", 'r', encoding='utf-8') as f:
         slownik=f.readlines()
     slownik=[x.strip() for x in slownik]
     print('Słownik gotowy')
-    print ('')
+    print('')
     return slownik
 
 def wprowadz_slowo():
     '''Wczytanie słowa od użytkownika'''
-    slowo=input('Podaj slowo do sprawdzenia lub 0, aby zakończyć: ')
-    print('')
+    slowo=input('Podaj słowo do sprawdzenia lub 0, aby zakończyć: ')
     slowo=slowo.lower()
+    print('')
     return slowo
 
 def wybor():
     '''Odpowiada za wybór funkcji programu'''
-    print ('Wybierz co chcesz zrobić')
-    print ('[1] Sprawdzić czy istnieje')
-    print ('[0] Wyjście')
-    print ('')
+    print('Wybierz, co chcesz zrobić')
+    print('[1] Sprawdź czy istnieje')
+    print('[2] Oblicz punkty za słowo')
+    print('[3] Sprawdź i oblicz')
+    print('[0] Wyjście')
+    print('')
     try:
         wybor=int(input('Wybór: '))
         print('')
-        if wybor>1:
+        if wybor>3 or wybor<0: #zmienić w przypadku dodania funkcji
             print('Nieprawidłowy wybór')
             print('')
             return None
@@ -35,21 +37,97 @@ def wybor():
         print('')
         return None
 
+def istnieje(slowo):
+    if sprawdz in slownik: 
+        return 'Słowo istnieje'
+    else:
+        return'Słowo nie istnieje'
+
+def premia_literowa(slowo,pkt,litery):
+    try:
+        ile=int(input('Liczba premii literowych: '))
+        print('')
+        if ile<0:
+            print('Nieprawidłowy wybór, premia nia naliczona')
+            print('')
+            return pkt
+    except ValueError:
+        print('')
+        print ('Nieprawidłowy wybór, premia nie naliczona')
+        print('')
+        return pkt
+    lit=[]
+    for i in range(0,ile):
+        lit.append(input('Podaj literę '+str(i+1)+' : '))
+        if lit[i] in slowo:
+            pkt+=litery[lit[i]]
+        else:
+            print('Brak takiej litery w słowie')
+    return pkt
+
+def punkty(slowo):
+    '''Oblicza punkty za słowo i premie'''
+    litery={'a':1, 'ą':5, 'b':3, 'c':2,
+            'ć':6, 'd':2, 'e':1, 'ę':5,
+            'f':5, 'g':3, 'h':3, 'i':1,
+            'j':3, 'k':2, 'l':2, 'ł':3,
+            'm':2, 'n':1, 'ń':7, 'o':1,
+            'ó':5, 'p':2, 'r':1, 's':1,
+            'ś':5, 't':2, 'u':3, 'w':1,
+            'y':2, 'z':1, 'ź':9, 'ż':5}
+    
+    pkt=0
+    for litera in slowo:
+        pkt+=litery[litera]
+    print('[1] Premia literowa')
+    print('[2] Premia słowna')
+    print('[3] Premia literowa i słowna')
+    print('[0] Brak premii')
+    try:
+        t=int(input('Premia: '))
+        print('')
+        if t<0:
+            print('Nieprawidłowy wybór, premia nia naliczona')
+            print('')
+            return pkt
+    except ValueError:
+        print('')
+        print ('Nieprawidłowy wybór, premia nie naliczona')
+        print('')
+        return pkt
+    if t==1:
+        pkt=premia_literowa(slowo,pkt,litery)
+    if t==2:
+        pkt=pkt*2
+        return pkt
+    if t==3:
+        pkt=premia_literowa(slowo,pkt,litery)
+        pkt=pkt*2
+        return pkt
+        
+    return pkt
 
 slownik=wczytaj_slownik()
 wyb=-1
 while wyb!=0:
     sprawdz=wprowadz_slowo()
     if sprawdz=='0':
-        print ('Koniec pracy')
         break
     wyb=wybor()
     if wyb==1:
-        if sprawdz in slownik: 
-            print ('Słowo istnieje')
+        print(istnieje(sprawdz))
+        print('')
+    if wyb==2:
+        print(punkty(sprawdz))
+        print('')
+    if wyb==3:
+        if istnieje(sprawdz)=='Słowo istnieje':
+            print('Słowo istnieje')
             print('')
+            print(punkty(sprawdz))
         else:
-            print ('Słowo nie istnieje')
+            print('Słowo nie istnieje')
             print('')
+print('Koniec pracy')
 
     
